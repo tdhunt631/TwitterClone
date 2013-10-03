d2f4a13ac04ce61dbc56a937050c2b197967a162
Travis Hunt <tdhunt631@gmail.com>
2013-10-02 22:55:46 -0600

    added microblog app

--- TwitterClone/settings.py
+++ TwitterClone/settings.py
@@ -1,4 +1,6 @@
 # Django settings for TwitterClone project.
+import os
+DIRNAME = os.path.dirname(__file__)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -11,8 +13,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'TwitterClone.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -29,7 +31,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Denver'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -121,9 +123,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+	'microblog',
+	'sorl.thumbnail',
 )
 
 # A sample logging configuration. The only tangible logging
---
+++ microblog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ModelForm, ModelChoiceField
+from sorl.thumbnail import ImageField
+
+class Profile(models.Model):
+	user = models.ForeignKey(User)
+	bio = models.TextField()
+	pic = ImageField(upload_to='profile_pictures')
+	following = models.ManyToManyField('Profile')
+
+	def __unicode__(self):
+		return self.user.username
+
+class Post(models.Model):
+	profile = models.ForeignKey(Profile)
+	message = models.CharField(max_length=140)
+	pub_date = models.DateTimeField(auto_now_add=True) 
